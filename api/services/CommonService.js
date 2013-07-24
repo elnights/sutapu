fd24462f04b980eedd2910cde8b48cd2a465dff7
Nick Sharshov <elnights@gmail.com>
2013-07-24 21:43:24 +0300

    ValidationMessages function update

--- api/services/CommonService.js
+++ api/services/CommonService.js
@@ -2,7 +2,7 @@ module.exports = {
 
   pickValidationMessages: function(err) {
     var result = [];
-    if (err.email || err.displayName || err.name) {
+    if (err.email || err.displayName || err.name || err.user || err.topic) {
       for (var field in err) {
         for (var rule in err[field]) {
           result.push(field + ': ' + err[field][rule].message);
