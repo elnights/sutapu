c90a776deedd4bc66e7ae4f52fcb86782c0f1e9f
Nick Sharshov <elnights@gmail.com>
2013-07-24 15:48:41 +0300

    Settings model

---
+++ api/models/Settings.js
@@ -0,0 +1,21 @@
+/**
+ * Settings
+ *
+ * @module      :: Settings
+ * @description :: Global application settings.
+ *
+ */
+
+module.exports = {
+
+  attributes: {
+  	
+  	name: {
+      type: 'string',
+      required: true
+    },
+    value: 'string'
+    
+  }
+
+};
