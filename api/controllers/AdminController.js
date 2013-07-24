caec582c69a75631299fab95dd4771eb397eaa29
Nick Sharshov <elnights@gmail.com>
2013-07-24 20:42:25 +0300

    Pass current user to admin view

--- api/controllers/AdminController.js
+++ api/controllers/AdminController.js
@@ -8,7 +8,14 @@
 module.exports = {
 
   index: function (req, res) {
-    res.view({_layoutFile: 'layout.ejs'});
+    if (req.user) {
+      User.findOne({
+        id: req.user.id
+      }).done(function(err, user) {
+        res.view({_layoutFile: 'layout.ejs', currentUser: JSON.stringify(user)});
+      });
+    }
+
   },
 
   login: function (req, res) {
