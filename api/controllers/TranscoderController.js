555ec1a5c1b7b550421c9351f987a64d8e7fd363
Nick Sharshov <elnights@gmail.com>
2013-07-24 18:28:40 +0300

    Transcoder controller created blank

---
+++ api/controllers/TranscoderController.js
@@ -0,0 +1,15 @@
+/**
+ * TranscoderController
+ *
+ * @module		:: Controller
+ * @description	:: Contains logic for handling requests.
+ */
+
+module.exports = {
+
+  create: function(req, res) {
+
+  }
+  
+
+};
