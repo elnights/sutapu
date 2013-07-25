927a64fe8daa420551dc514ecbe2fb733d583f91
Nick Sharshov <elnights@gmail.com>
2013-07-25 03:10:22 +0300

    fillPostsWithUserAndTopic made service function

--- api/controllers/PostController.js
+++ api/controllers/PostController.js
@@ -7,58 +7,6 @@
 
 var async = require('async');
 
-function fillPostsWithUserAndTopic(posts, callback) {
-  if (posts instanceof Array) {
-    async.map(posts, function(post, result) {
-      async.parallel([
-        function(callback){
-          User.findOneById(post.user).done(function(err, user) {
-            callback(null, user);
-          });
-        },
-        function(callback) {
-          Topic.findOneById(post.topic).done(function(err, topic) {
-            topic.user = {id: topic.user};
-            callback(null, topic);
-          });
-        }
-      ],
-        function(err, results){
-          // the results array will equal ['one','two'] even though
-          // the second function had a shorter timeout.
-          post.user = results[0];
-          post.topic = results[1];
-          result(err, post);
-        }
-      );
-    }, function(err, posts) {
-      callback(posts);
-    });
-  } else {
-    var post = posts;
-    async.parallel([
-      function(callback){
-        User.findOneById(post.user).done(function(err, user) {
-          callback(null, user);
-        });
-      },
-      function(callback) {
-        Topic.findOneById(post.topic).done(function(err, topic) {
-          callback(null, topic);
-        });
-      }
-    ],
-      function(err, results){
-        // the results array will equal ['one','two'] even though
-        // the second function had a shorter timeout.
-        post.user = results[0];
-        post.topic = results[1];
-        callback(post);
-      }
-    );
-  }
-}
-
 module.exports = {
 
   /* e.g.
@@ -99,7 +47,7 @@ module.exports = {
     }
 
     def.exec(function(err, posts) {
-      fillPostsWithUserAndTopic(posts, function(posts) {
+      CommonService.fillPostsWithUserAndTopic(posts, function(posts) {
         res.json(posts);
       });
     });
@@ -116,7 +64,7 @@ module.exports = {
           }, 500);
         } else {
           if (post) {
-            fillPostsWithUserAndTopic(post, function(fullPost) {
+            CommonService.fillPostsWithUserAndTopic(post, function(fullPost) {
               res.json(fullPost);
             });
           } else {
@@ -134,7 +82,7 @@ module.exports = {
           description: err
         }, 500);
       } else {
-        fillPostsWithUserAndTopic(posts, function(fullPost) {
+        CommonService.fillPostsWithUserAndTopic(posts, function(fullPost) {
           res.json(fullPost);
         });
       }
