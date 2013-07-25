2d082657914e7c88393493aef3b59a0e1f1ca75c
bartezic <bartezic@gmail.com>
2013-07-25 03:36:22 +0300

    admin: added lists, pagination to list ...

--- api/controllers/AdminController.js
+++ api/controllers/AdminController.js
@@ -3,19 +3,104 @@
  *
  * @module		:: Controller
  * @description	:: Contains logic for handling requests.
+
  */
+
+async = require('async');
+
+function lists(res, req, model, callback){
+  if (req.user) {
+    var perPage = 5, pagin,
+        page = req.param('page') ? parseInt(req.param('page')) : 1;
+
+    model.count().done(function(err, count){
+      pagin = {
+        max: 10,
+        current: page,
+        count: count,
+        limit: perPage,
+        offset: (page-1) * perPage,
+        total: Math.ceil(count/perPage)
+      };
+
+      model.find()
+        .limit(pagin.limit)
+        .skip(pagin.offset)
+        .sort('id DESC')
+        .done(function(err, items) {
+          User.findOne(req.user.id).done(function(err, user) {
+            callback(user, items, pagin)
+          });
+        }); 
+    });
+  } else {
+    res.redirect('/admin/login');
+  }
+};
 
 module.exports = {
 
   index: function (req, res) {
     if (req.user) {
-      User.findOne({
-        id: req.user.id
-      }).done(function(err, user) {
-        res.view({_layoutFile: 'layout.ejs', currentUser: JSON.stringify(user)});
+      User.findOne(req.user.id).done(function(err, user) {
+        async.map([User, Topic, Post], function(item, callback) {
+          item.count().done(function(err, item) {
+            callback(null, item)
+          });
+        }, function(err, results) {
+          res.view({
+            _layoutFile: 'layout.ejs', 
+            currentUser: user,
+            anal: {
+              user: results[0],
+              topic: results[1],
+              post: results[2]
+            },
+            menu: 'index'
+          });
+        });
       });
+    } else {
+      res.redirect('/admin/login');
     }
+  },
 
+  users: function (req, res) {
+    lists(res, req, User, function(user, items, pagin){
+      res.view({
+        _layoutFile: 'layout.ejs',
+        currentUser: user,
+        items: items,
+        pagin: pagin,
+        menu: 'Users'
+      });
+    });
+  },
+
+  topics: function (req, res) {
+    lists(res, req, Topic, function(user, items, pagin){
+      res.view({
+        _layoutFile: 'layout.ejs',
+        currentUser: user,
+        items: items,
+        pagin: pagin,
+        menu: 'Topics'
+      });
+    });
+  },
+
+  posts: function (req, res) {
+    lists(res, req, Post, function(user, items, pagin){
+      CommonService.fillPostsWithUserAndTopic(items, function(fullItems) {
+        res.view({
+          _layoutFile: 'layout.ejs',
+          currentUser: user,
+          items: fullItems,
+          pagin: pagin,
+          menu: 'Posts'
+        });
+      });
+    });
   },
 
   login: function (req, res) {
--- assets/admin/js/app.js
+++ assets/admin/js/app.js
@@ -1,16 +1,82 @@
-window.sutatu = {};
+window.sutatu = {
+  alert: function(type, msg){
+    $().toastmessage('showToast',{
+      text     : msg,
+      position : 'top-right',
+      type     : type
+    });
+  }
+};
 
-window.sutatu.login = {
+window.sutatu.auth = {
   initHandlers: function() {
-    
+    var _ = this;
+
+    _.elems.signout.click(function() {
+      $.ajax({
+        url: '/auth/logout',
+        dataType: 'JSON'
+      }).done(function(res) {
+        if(res.result == 'ok'){
+          window.location.reload();
+        } else {
+          _.root.alert('warning', 'Oops...');
+        }
+      }).fail(function(res) {
+        _.root.alert('error', res.responseJSON.code +' '+ res.responseJSON.description);
+      });
+    });
+
+    _.elems.login.click(function(e) {
+      e.preventDefault();
+      
+      $.ajax({
+        url: '/auth/local',
+        dataType: 'JSON',
+        data: {
+          email: _.elems.email.val(),
+          password: _.elems.password.val()
+        }
+      }).done(function() {
+        window.location.href = '/admin/';
+      }).fail(function(res) {
+        _.root.alert('error', res.responseJSON.code +' '+ res.responseJSON.description);
+      });
+    });
   },
   init: function(){
+    this.root = window.sutatu;
     this.elems = {
+      signout: $('.signout'),
+      login: $('.form-signin .login'),
+      email: $('.form-signin input[name=email]'),
+      password: $('.form-signin input[name=password]')
+    };
 
-    }
+    this.initHandlers();
+  }
+};
+
+window.sutatu.moment = {
+  formate: function() {
+    var _ = this;
+
+    $.each(_.elems.timesForMoment, function(i, el){
+      var time = moment($(el).text(), "YYYYMMDDHHmm Z").fromNow();
+      $(el).text(time);
+    });
+  },
+  init: function(){
+    this.root = window.sutatu;
+    this.elems = {
+      timesForMoment: $('.for-moment')
+    };
+
+    this.formate();
   }
 };
 
 $(function(){
-  window.sutatu.login.init()
+  window.sutatu.auth.init();
+  window.sutatu.moment.init();
 });
